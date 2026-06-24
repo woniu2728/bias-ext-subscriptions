ec2d3f517d07d4ec262554b6e5733821197a8264
woniu <2463396298@qq.com>
2026-06-24 12:14:39 +0800

    Add apps.py + django_migrations

---
+++ bias_ext_subscriptions/backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class SubscriptionsExtensionConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bias_ext_subscriptions.backend"
+    label = "subscriptions"
